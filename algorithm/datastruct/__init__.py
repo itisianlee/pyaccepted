28da234b63fdec08f84e9f78431e1889a8ddc7d9
ianleedream <ianleedream@gmail.com>
2020-07-21 10:40:12 +0800

    del space line

--- algorithm/datastruct/__init__.py
+++ algorithm/datastruct/__init__.py
@@ -1,1 +1,2 @@
 from .listnode import ListNode, createlink, traverse
+from .tree import TreeNode, Tree
--- algorithm/datastruct/listnode.py
+++ algorithm/datastruct/listnode.py
@@ -1,9 +1,7 @@
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-    
 
 
 def createlink(arr=None):
